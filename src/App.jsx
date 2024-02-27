a420576738c548e31791266799e6782c9f1c6527
mharoon-dev <haroon.aghani@gmail.com>
2024-02-27 19:55:38 +0500

    create a project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    <>
+    <Friends />
+    <AddFriend />
+    <SplitABill />
+    </>
+}
+
+export default App;
+
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={a1da7024b3993a3549bb57305e9e57ca}
